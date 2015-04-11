899c386c3fec4dec636ba988ca3e17172a1f147a
Indu Mohanan <imohanan@usc.edu>
2015-04-11 16:58:01 -0700

    Generates json file with products having 5 or more reviews

---
+++ amazonReviews/getJson.py
@@ -0,0 +1,34 @@
+import gzip 
+import json as simplejson 
+
+
+def parse(filename):
+    f = gzip.open(filename, 'r')
+    entry = {}
+    for l in f: 
+        l = l.strip() 
+        colonPos = l.find(':')
+        if colonPos == -1: 
+            yield entry 
+            entry = {} 
+            continue
+        eName = l[:colonPos] 
+        rest = l[colonPos+2:] 
+        entry[eName] = rest 
+    yield entry
+
+
+productReview={}
+for e in parse("Watches.txt.gz"):
+    if "product/productId" not in e: continue
+    if e["product/productId"] not in productReview: productReview[e["product/productId"]]=[e]
+    else: 
+        reviews=productReview[e["product/productId"]]
+        reviews.append(e)
+        productReview[e["product/productId"]]=reviews
+for e in parse("Watches.txt.gz"):
+    if "product/productId" not in e: continue
+    reviews=productReview[e["product/productId"]]
+    if len(reviews) > 3:
+        print(simplejson.dumps(e))
+
