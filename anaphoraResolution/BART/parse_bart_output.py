0fa91a42cb0fe07a6c91640485e399328123439d
Indu Mohanan <imohanan@usc.edu>
2015-04-18 21:31:37 -0700

    Code to replace text with corefernece noun phrase

---
+++ anaphoraResolution/BART/parse_bart_output.py
@@ -0,0 +1,84 @@
+from bs4 import BeautifulSoup
+
+class Parser:
+
+    def __init__(self, filename):
+        with open(filename,"r") as output_file:
+            self.text=output_file.read()    
+
+    def find_corefs(self,data):
+         for tag in data.find_all('coref'):
+            coref_id= tag['set-id']
+            if coref_id not in self.coref: self.coref[coref_id]=[]
+            coref_words=[]
+            for word in tag.find_all('w'):
+                pos=word['pos']
+                text=word.getText()
+                coref_words.append((text,pos))
+            self.coref[coref_id].append(coref_words)
+
+    def replace_coref(self, parent):
+        coref_id=parent['set-id']  
+        coref_sentences=self.coref[coref_id]
+        noun_phrase=[]
+        noun_phrase_found=False
+        for sentence in coref_sentences:
+            for word in sentence:
+                if word[1]=='nn': 
+                    return [sen_tuple[0] for sen_tuple in sentence]
+                    noun_phrase_found=True
+                    break
+        if noun_phrase_found==False: 
+            words=parent.find_all('w')
+            words=[word.get_text() for word in words]
+            return words
+            
+                
+
+    def parse(self):
+        self.coref={}
+        data=BeautifulSoup(self.text)
+        # 1. get coreferences and save it in a dictionary
+        self.find_corefs(data)
+        # 2. create a new list of words
+        # append word
+        # if word is coref without nn replace it with a noun phrase
+        all_words=data.find_all('w')
+        final_sentence=[]
+        word_count=0
+        while word_count < len(all_words):
+            if 'set-id' in all_words[word_count].parent.attrs: 
+                new_words=self.replace_coref(all_words[word_count].parent)    
+                for new_word in new_words:
+                    final_sentence.append(new_word) 
+                while ('set-id' in all_words[word_count+1].parent.attrs) and (word_count+1 < len(all_words))  : word_count+=1   
+            else :  final_sentence.append(all_words[word_count].getText())      
+            word_count+=1
+        print final_sentence
+        return 
+
+
+
+
+        for tag in data.find_all('coref'):
+            #del tag['set-id']
+            tag.extract()
+            #noun_phrase=tag.find_all('w',{"pos":"nn"})
+            #if len(noun_phrase) ==0:    print(tag)
+            #else: print(noun_phrase)
+        print(data)        
+        return 
+        #print(self.coref)    
+        text=[] 
+        for sentence in data.find_all('s'):
+            words=sentence.find_all('w')
+            text.extend([word.getText() for word in words])
+        print(text)
+            #for word in words:
+                #print(word)
+        #for word in data.find_all('w'):
+            #print word.text
+
+if __name__=="__main__":
+    parser=Parser("output.xml")
+    parser.parse()
