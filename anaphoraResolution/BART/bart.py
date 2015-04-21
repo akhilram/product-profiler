90bc8ef57fef451cf89a5db601796f1416b430fe
Indu Mohanan <imohanan@usc.edu>
2015-04-21 11:38:30 -0700

    Accepting a file with multiple reviws

--- anaphoraResolution/BART/bart.py
+++ anaphoraResolution/BART/bart.py
@@ -23,7 +23,7 @@ def get_coreferences(input_filepath, host=BART_SERVER):
     Takes a plain text file as input, pushes it to a running BART
     coreference server and returns a string that contains an inline XML
     representation of the input with coreferences added.
-    """
+        """
     assert os.path.isfile(input_filepath), \
         "File doesn't exist: {}".format(input_filepath)
     with open(input_filepath, 'r') as input_file:
@@ -33,14 +33,24 @@ def get_coreferences(input_filepath, host=BART_SERVER):
 
 
 if __name__ == '__main__':
+    with open(sys.argv[1],'r') as  input_file:
+        lines=input_file.readlines()
+    count=0
+    while count< len(lines):
+        name=sys.argv[1]+str(count)
+        with open('Input/'+,'w') as temp_file:
+            temp_file.write(lines[count])
+        count+=1
     if len(sys.argv) != 3:
         sys.stderr.write(
             'Usage: {0} input_file output_file\n'.format(sys.argv[0]))
         sys.exit(1)
     else:
-        input_filepath = sys.argv[1]
-        output_string = get_coreferences(input_filepath)
+        for filename in sorted(os.listdir('Input')):
+        
+            input_filepath = 'Input/'+filename
+            output_string = get_coreferences(input_filepath)
 
-        output_filepath = sys.argv[2]
-        with open(output_filepath, 'w') as output_file:
-            output_file.write(output_string)
+            output_filepath = 'Output/'+filename+'.xml'
+            with open(output_filepath, 'w') as output_file:
+                output_file.write(output_string)
