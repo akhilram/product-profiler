da6dd5ae575f036fe1591d7caa00a78066a9bdf4
npmajisha <npmajisha@gmail.com>
2015-04-27 18:35:42 -0700

    Basic php interaion for feature opinion

--- Interface/get-rating.php
+++ Interface/get-rating.php
@@ -1,5 +1,23 @@
 <?php
 
-    echo $_GET["features"];
+
+    $inputFilterFeatures = array('band','crystal','dial','strap');
+	//$inputFilterFeatures = $_GET["features"];
+
+    //$inputFeaturesText = $_GET["productidvalue"].'FilteredFeatures.txt';
+    $inputFeaturesText = 'Features.txt';
+    //$inputAnaphora = $_GET["productidvalue"].'ReviewAnaphoraRes.txt';
+    $inputAnaphora = "reviews_output.txt";
+	foreach ($inputFilterFeatures as $feature) {
+		file_put_contents($inputFeaturesText, $feature."\n", FILE_APPEND);
+	}
+
+    //$outputSentiments = $_GET["productidvalue"] . 'PhraseOutput.txt';
+    $outputSentiments = 'PhraseOutput.txt';
+
+	$getOpinionPhrases = 'java -jar PhraseSentimentExtractor.jar '.$inputFeaturesText.' ' .$inputAnaphora . ' ' . $outputSentiments;
+
+	$result = shell_exec($getOpinionPhrases);
+		
 
 ?>
